3aed98abab064d97b84e082ea170d31c5334e455
Semiha Özdemir <semihaozdemir34@gmail.com>
2021-11-24 19:03:17 +0300

    Add project files.

---
+++ ödev2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ödev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ÖDEV2:KULLANICIDAN ÖĞRENCİ SAYISI İSTENECEK. ÖĞRENCİ SAYISI KADAR VİZE VE FİNAL NOTLARI İSTENECEK.ÖĞRENCİ ORTALAMASI BULUNACAK.
+            //1)EN YÜKSEK ORTALAMAYI BULUN
+            //2)EN DÜŞÜK ORTALAMAYI BULUN
+            //3)GENEL ORTALAMAYI BULUN
+
+            // 1,2,3 maddeleri eksik.
+
+
+            Console.WriteLine("lütfen öğrenci sayısını giriniz.");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            double enb = 0;
+            double enk = 100;
+            double genelort = 0;
+            double toplam = 0;
+
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine($"lütfen {i + 1} . öğrencinin vize notunu giriniz.");
+                int vize = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"lütfen {i + 1} . öğrencinin final notunu giriniz.");
+                int final = Convert.ToInt32(Console.ReadLine());
+
+                double ort = (vize * 0.4) + (final * 0.6);
+
+                if(ort>enb)
+                {
+                    enb=ort;
+                }
+                if(ort<enk)
+                {
+                    enk=ort;
+                }
+
+                toplam += ort;
+            }
+
+
+            genelort = toplam / n;
+            Console.WriteLine($"en yüksek ortalama= {enb}");
+            Console.WriteLine($"en düşük ortalama= {enk}");
+            Console.WriteLine($"genel ortalama= {genelort}");
+
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ ödev_20211107/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ödev_20211107
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ÖDEV1: ÖRNEKTE OLDUĞU GİBİ KULLANICI ADI ADMİN VE ŞİFRE 123 İSE HOŞGELDİN ADMİN MESAJI VERİLECEK. EĞER KULLANICI BİLGİLERİ HATALI GİRERSE
+            //KULLANICIYA 3 ADET HAK VERİLECEK. 3 KERE YANLIŞ GİRERSEDE UYGULAMA SONLANDIRILACAK.PERŞEMBE 24:00'E KADAR GÖNDERİLECEK.GİTHUB ÜZERİNDEN.LİNKİ HOCAYA AT.
+
+            //ÖDEV2:KULLANICIDAN ÖĞRENCİ SAYISI İSTENECEK. ÖĞRENCİ SAYISI KADAR VİZE VE FİNAL NOTLARI İSTENECEK.ÖĞRENCİ ORTALAMASI BULUNACAK.
+            //1)EN YÜKSEK ORTALAMAYI BULUN
+            //2)EN DÜŞÜK ORTALAMAYI BULUN
+            //3)GENEL ORTALAMAYI BULUN
+
+            int hak_sayisi = 3;
+
+            while(true)
+            {
+                Console.WriteLine("kullanıcı adınızı giriniz.");
+                string kullaniciadi = Console.ReadLine();
+
+                Console.WriteLine("şifrenizi giriniz.");
+                string sifre = Console.ReadLine();
+
+                if (kullaniciadi=="admin" && sifre=="123")
+                {
+                    Console.WriteLine("hoşgeldin admin");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("kullanıcı adınız veya şifreniz hatalı!");
+
+                    if (hak_sayisi>0)
+                    {
+                        hak_sayisi -= 1;
+                    }
+                    if (hak_sayisi==0)
+                    {
+                        Console.WriteLine("hakkınız dolmuştur.Erişim izniniz iptal edilmiştir.");
+                        break;
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
